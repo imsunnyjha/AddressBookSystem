73ef078a94c485ec6e0e10f4c153cc7ced474a14
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-24 20:52:30 +0530

    [SUNNY]Add.UseCase8 to search a person by city or state

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        List<Contact> contactList;
+
+
+        public AddressBook()
+        {
+            contactList = new List<Contact>();
+
+        }
+        public string AddContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            if (CheckName(firstName, lastName) == false)
+            {
+                Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, emailId);
+                contactList.Add(contact);
+                return "Details Added Successfully!";
+            }
+            return "Name already exists!";
+        }
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                {
+                    c.lastName = lastName;
+                    c.address = address;
+                    c.city = city;
+                    c.state = state;
+                    c.zipCode = zipCode;
+                    c.phoneNo = phoneNo;
+                    c.emailId = emailId;
+
+                    return;
+                }
+            }
+        }
+        public void RemoveContact(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName) && c.lastName.Equals(lastName))
+                {
+                    contactList.Remove(c);
+
+                    return;
+                }
+            }
+        }
+        public bool CheckName(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName) && c.lastName.Equals(lastName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void SearchContactByCityOrState(string cityOrState)
+        {
+
+            foreach (var contact in contactList)
+            {
+                if (contact.city == cityOrState || contact.state == cityOrState)
+                {
+                    Console.WriteLine("Name :" + contact.firstName + " " + contact.lastName + "\nAddress :" + contact.address + "   ZipCode :" + contact.zipCode + "\nPhone No :" + contact.phoneNo + "   emailId :" + contact.emailId);
+                }
+            }
+
+        }
+    }
+}
--- AddressBookSystem/Contact.cs
+++ AddressBookSystem/Contact.cs
@@ -7,223 +7,30 @@ namespace AddressBookSystem
 {
     public class Contact 
     {
-        List <Contact> contacts = new List <Contact>();
-        // Creating setter and getter for each property  
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string address { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public int zipCode { get; set; }
-        public long phoneNumber { get; set; }
-        public string emailId { get; set; }
+        public string firstName, lastName, address, city, state, zipCode, phoneNo, emailId;
 
         public Contact()
         {
+            firstName = "";
+            lastName = "";
+            address = "";
+            city = "";
+            state = "";
+            zipCode = "";
+            phoneNo = "";
+            emailId = "";
+        }
+        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNo = phoneNo;
+            this.emailId = emailId;
+        }
 
-        }
-        //Function to add new addressBook
-        public void DisplayAddressBook()
-        {
-            if (contacts.Count == 0)
-                Console.WriteLine("No Contacts to display");
-            else
-            {
-                Console.WriteLine("\nDisplaying Contacts - \nFirst Name\tLast Name\tAddress\tCity\tState\tZIP Code\tPhone Number\tEmailId");
-                foreach (Contact contact in contacts)
-                {
-                    Console.WriteLine(contact.firstName + "\t\t" + contact.lastName + "\t\t" + contact.address + "\t" + contact.city + "\t" + contact.state + "\t" + contact.zipCode + "\t\t" + contact.phoneNumber + "\t\t" + contact.emailId);
-                }
-            }
-        }
-        //Function to add new contact
-        public void AddNewContact()
-        {
-            bool check = false;
-            Contact contact = new Contact();
-            //Firstname Input
-            string namePattern = "^[a-zA-Z ]+$";
-            Console.WriteLine("Enter First Name: ");
-            string firstName = Console.ReadLine();
-            if(!Regex.IsMatch(firstName,namePattern))
-                throw new Exception("Name should be alphabetical!");
-            else
-                contact.firstName = firstName;
-            //Lastname Input
-            Console.WriteLine("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-            if (!Regex.IsMatch(lastName, namePattern))
-                throw new Exception("Name should be alphabetical!");
-            else
-                contact.lastName = lastName;
-
-            ///<summary>
-            ///Checking for duplicate entry
-            ///</summary>
-            foreach (Contact con in contacts)
-            {
-                if (con.firstName.ToUpper().Equals(firstName.ToUpper()) && con.lastName.ToUpper().Equals(lastName.ToUpper()))
-                {
-                    Console.WriteLine("Name exists in the system! Input another name!");
-                    check = true;
-                    break;
-                }
-            }
-            if (check==false)
-            {
-                //Address Input
-                string addressPattern = "^[a-zA-Z0-9 ]+$";
-                Console.WriteLine("Enter Address:");
-                string address = Console.ReadLine();
-                if (!Regex.IsMatch(address, addressPattern))
-                    throw new Exception("Address should be in proper format!");
-                else
-                    contact.address = address;
-                //City Input
-                Console.WriteLine("Enter City: ");
-                string city = Console.ReadLine();
-                if (!Regex.IsMatch(city, namePattern))
-                    throw new Exception("City name should be in proper format!");
-                else
-                    contact.city = city;
-                //State Input
-                Console.WriteLine("Enter State: ");
-                string state = Console.ReadLine();
-                if (!Regex.IsMatch(state, namePattern))
-                    throw new Exception("State should be in proper format!");
-                else
-                    contact.state = state;
-                //Zip code Input
-                string zipPattern = "[1-9][0-9]{5}";
-                Console.WriteLine("Enter ZIP Code: ");
-                string zip = Console.ReadLine();
-                if (!Regex.IsMatch(zip, zipPattern))
-                    throw new Exception("ZIP Code should be a 6 digit number");
-                else
-                    contact.zipCode = int.Parse(zip);
-                //Phone Number Input
-                string phonePattern = "[1-9][0-9]{9}";
-                Console.Write("Enter Phone Number: ");
-                string phNumber = Console.ReadLine();
-                if (!Regex.IsMatch(phNumber, phonePattern))
-                    throw new Exception("Phone number must be a 10 digit number!");
-                else
-                    contact.phoneNumber = long.Parse(phNumber);
-
-                //EmailId Input
-                string mailPattern = @"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-                Console.Write("Enter Email Id: ");
-                string mailId = Console.ReadLine();
-                if (!Regex.IsMatch(mailId, mailPattern))
-                    throw new Exception("Check Mail address");
-                else
-                    contact.emailId = mailId;
-                //Contact Added
-                contacts.Add(contact);
-                Console.WriteLine("New Contact added successfully");
-            }
-        }
-        //Function to display contact
-        public void DisplayContact()
-        {
-            Console.WriteLine("\nDisplaying Contacts - \nFirst Name\tLast Name\tAddress\tCity\tState\tZIP Code\tPhone Number\tEmailId");
-            foreach (Contact contact in contacts)
-            {
-                Console.WriteLine(contact.firstName + "\t\t" + contact.lastName + "\t\t" + contact.address + "\t" + contact.city + "\t" + contact.state + "\t" + contact.zipCode + "\t\t" + contact.phoneNumber + "\t\t" + contact.emailId);
-            }
-        }
-        //Function to Edit Contact
-        public void EditContact()
-        {
-            Console.WriteLine("Enter the first name of the person: ");
-            string fName = Console.ReadLine();
-            foreach (Contact c in contacts)
-            {
-                if (c.firstName.Equals(fName))
-                {
-                    int editChoice = 0;
-                    while (editChoice < 9)
-                    {
-                        Console.WriteLine("Enter the following choice");
-                        Console.WriteLine("1. Edit First Name");
-                        Console.WriteLine("2. Edit Last Name");
-                        Console.WriteLine("3. Edit Address");
-                        Console.WriteLine("4. Edit City");
-                        Console.WriteLine("5. Edit State");
-                        Console.WriteLine("6. Edit Zip");
-                        Console.WriteLine("7. Edit Phone Number");
-                        Console.WriteLine("8. Edit E-mail");
-                        Console.WriteLine("9. Exit");
-                        Console.WriteLine("Enter your choice: ");
-                        editChoice = Convert.ToInt32(Console.ReadLine());
-
-                        switch (editChoice)
-                        {
-                            case 1:
-                                Console.WriteLine("1. Edit First Name");
-                                string fname = Console.ReadLine();
-                                c.firstName = fname;
-                                break;
-                            case 2:
-                                Console.WriteLine("1. Edit Last Name");
-                                string lname = Console.ReadLine();
-                                c.lastName = lname;
-                                break;
-                            case 3:
-                                Console.WriteLine("1. Edit Address Name");
-                                string aAddress = Console.ReadLine();
-                                c.address = aAddress;
-                                break;
-                            case 4:
-                                Console.WriteLine("1. Edit City Name");
-                                string cCity = Console.ReadLine();
-                                c.city = cCity;
-                                break;
-                            case 5:
-                                Console.WriteLine("1. Edit State");
-                                string sState = Console.ReadLine();
-                                c.state = sState;
-                                break;
-                            case 6:
-                                Console.WriteLine("1. Edit Zip");
-                                int zCode = Convert.ToInt32(Console.ReadLine());
-                                c.zipCode = zCode;
-                                break;
-                            case 7:
-                                Console.WriteLine("1. Edit Phone Number");
-                                long phNum = Convert.ToInt64(Console.ReadLine());
-                                c.phoneNumber = phNum;
-                                break;
-                            case 8:
-                                Console.WriteLine("1. Edit Email");
-                                string mail = Console.ReadLine();
-                                c.emailId = mail;
-                                break;
-                        }
-                    }
-                    Console.WriteLine("Edited Successfully");
-                }
-                else
-                {
-                    Console.WriteLine("Enter a valid name");
-                }
-            }
-        }
-        //Function to Delete Contact 
-        public void DeleteContact()
-        {
-            Console.WriteLine("Enter the first name of the person: ");
-            string fName = Console.ReadLine();
-            List<Contact> con = new List<Contact>();
-            foreach (Contact c in contacts)
-            {
-                if (c.firstName.Equals(fName))
-                {
-                    con.Add(c);             
-                }
-            }
-            contacts.RemoveAll(i => con.Contains(i));
-            Console.WriteLine("Contact Removed Successfully");
-        }
     }
 }
